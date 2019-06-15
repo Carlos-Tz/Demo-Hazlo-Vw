08aea6b093ae4d95d9a8e00ec8b1b784fee08ce3
Charly921 <44937626+Charly921@users.noreply.github.com>
2019-06-15 15:57:58 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyBEvr714luHjTE8Beh1FJsmUikgxGqT7OA',
-    authDomain: 'hazlo-volkswagen.firebaseapp.com',
-    databaseURL: 'https://hazlo-volkswagen.firebaseio.com',
-    projectId: 'hazlo-volkswagen',
-    storageBucket: 'hazlo-volkswagen.appspot.com',
-    messagingSenderId: '631335703071',
-    appId: '1:631335703071:web:29bbda39415feca7'
-  }
+  production: false
 };
 
 /*
