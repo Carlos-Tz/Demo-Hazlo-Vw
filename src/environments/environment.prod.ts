54f03b529103934b3b09eff54237bce702dc778c
Charly921 <44937626+Charly921@users.noreply.github.com>
2019-06-15 15:57:25 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyBEvr714luHjTE8Beh1FJsmUikgxGqT7OA',
-    authDomain: 'hazlo-volkswagen.firebaseapp.com',
-    databaseURL: 'https://hazlo-volkswagen.firebaseio.com',
-    projectId: 'hazlo-volkswagen',
-    storageBucket: 'hazlo-volkswagen.appspot.com',
-    messagingSenderId: '631335703071',
-    appId: '1:631335703071:web:29bbda39415feca7'
-  }
+  production: true
 };
